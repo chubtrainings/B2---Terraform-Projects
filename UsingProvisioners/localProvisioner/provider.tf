718d30bd3b259e1a9e2768498bda5ec8db589d45
chubtrainings <147708673+chubtrainings@users.noreply.github.com>
2024-03-23 21:14:21 +0530

    Create provider.tf

---
+++ UsingProvisioners/localProvisioner/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  #shared_credentials_file = "C:/tf_user/.aws/creds"
+   access_key = "xxxxxxx"            # AWS  account Access Key
+   secret_key = "xxxxxxxxxxxxxxx"   # AWS  account Secret Key
