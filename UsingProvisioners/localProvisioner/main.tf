017ac33d214916855488d17396600d4fa9429850
chubtrainings <147708673+chubtrainings@users.noreply.github.com>
2024-03-23 21:11:54 +0530

    Create main.tf

---
+++ UsingProvisioners/localProvisioner/main.tf
@@ -0,0 +1,94 @@
+
+
+# Creating securitygroup using Terraform
+resource "aws_security_group" "TF_SG" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id = "vpc-05272259ac938cc4c"        # Existing Default VPC in region us-east-1
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SG"
+  }
+}
+
+# Create keypair 
+
+resource "aws_key_pair" "TF_key" {
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "TF_key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "tfkey"
+   # file_permission = "0400"
+} 
+
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-021e5ab7e31e9ba32"       # Amazon Linux AMI - us-east-1
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.TF_SG.name]
+  key_name = "TF_key"
+  depends_on = [aws_key_pair.TF_key]
+
+  
+  tags = {
+    Name = "Terraform Ec2"
+  }
+
+# terraform provisioners
+
+ provisioner "file" {
+    source      = "./test-file.txt"
+    destination = "/home/ec2-user/test-file.txt"
+  }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("C:/Users/aksha/projects - trfrm/Creating Security Group in terraform/tfkey")
+      timeout     = "4m"
+   }
+
+}
