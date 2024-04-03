c7f8f3d658ffd65093a0544fdb5729ac9c4df34b
chubtrainings <147708673+chubtrainings@users.noreply.github.com>
2024-04-03 05:59:34 +0530

    Update Provider.tf

--- CreatingMultipleReources-Modules/Provider.tf
+++ CreatingMultipleReources-Modules/Provider.tf
@@ -1,7 +1,7 @@
 /* provider "aws" {
    region     = "ap-south-1" 
-   access_key = "AKIAZI2LGPJVHHY4CROS"            # Ajay account
-   secret_key = "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"   # Ajay account
+   access_key = "xxxxxxxxxx"            
+   secret_key = "xxxxxxxxxxxx"   
     
 }   */
 
@@ -10,6 +10,6 @@
 provider "aws" {
   region     = "us-east-1"
   #shared_credentials_file = "C:/tf_user/.aws/creds"
-   access_key = "AKIAU6GDXYEK7D66NC5Y"            # prathi account
-   secret_key = "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"   # prathi account
+   access_key = xxxxxxxxxx"            # prathi account
+   secret_key = "xxxxxxxxxxxxx"   # prathi account
 }
